ea1ccae235a00db36692edb514a73f553447a303
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-09-07 13:40:50 +0530

    Update index.js

--- apps/testskill/index.js
+++ apps/testskill/index.js
@@ -2,7 +2,7 @@ module.change_code = 1;
 'use strict';
 
 var alexa = require( 'alexa-app' );
-var app = new alexa.app( 'my-skill' );
+var app = new alexa.app( 'test-skill' );
 
 
 app.launch( function( request, response ) {
