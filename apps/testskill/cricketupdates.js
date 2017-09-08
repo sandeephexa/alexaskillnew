58ab746da71a2cf83a25229736a00e6fc4e5df5e
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-09-08 11:49:56 +0530

    Update cricketupdates.js

--- apps/testskill/cricketupdates.js
+++ apps/testskill/cricketupdates.js
@@ -1,5 +1,5 @@
 const request = require('request-sync');
-let datares;
+
 module.exports = function () {
     return JSON.parse(request({
         uri: "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=3058b62b8b2d4eda9a246510fccbf556" ,
